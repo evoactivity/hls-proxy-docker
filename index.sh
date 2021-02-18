dd8cd49ae2fc4c579bd84c91d3fc0d2c0f6b3ad7
MapGuy11 <43685307+MapGuy11@users.noreply.github.com>
2021-02-18 13:07:28 -0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo '------ DOWNLOADING HLS PROXY -------'
-wget https://www.hls-proxy.com/downloads/7.4.8/hls-proxy-7.4.8.linux-x64.zip -O /var/tmp/hlsproxy.zip
+wget https://www.hls-proxy.com/downloads/7.5.0/hls-proxy-7.5.0.linux-x64.zip -O /var/tmp/hlsproxy.zip
 echo '------ UNZIP HLS PROXY -------'
 mkdir -p /var/tmp/hlsp/
 unzip -o /var/tmp/hlsproxy.zip -d /var/tmp/hlsp/
