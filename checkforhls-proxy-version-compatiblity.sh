90b8b8ff1b6ba5ead573fcf591fc5cf8854d95e9
MapGuy11 <43685307+MapGuy11@users.noreply.github.com>
2021-02-24 18:47:52 -0500

    Create checkforhls-proxy-version-compatiblity.sh

---
+++ checkforhls-proxy-version-compatiblity.sh
@@ -0,0 +1,8 @@
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    # YOUR_JUST_ONCE_LOGIC_HERE
+else
+    echo "-- Not first container startup --"
+fi
